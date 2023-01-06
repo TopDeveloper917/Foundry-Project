2971b42d5aff067821dfcad18279ce6b043e97e5
topdeveloper917 <jamessmith0917topdev@gmail.com>
2023-01-06 08:59:43 +0800

    Create GodwokenMultipleNFT.t.sol

---
+++ test/GodwokenMultipleNFT.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+contract GodwokenMultipleNFTTest is Test {
+    
+    /// @dev The shop owner
+    address public constant OWNER = address(999);
+    
+}
